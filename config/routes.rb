f5ecc7f911df9fc5008ebb9c55be7489d4a1905b
Doragon-RH <r216af@icloud.com>
2025-05-06 11:48:36 +0900

    テストコードの記述、ルートの記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  # Define the root path
+  root 'hello#index'
   resources :books, only: [:index]
 end
--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -4,4 +4,10 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "should get index" do
+    get books_url
+    assert_response :success
+    parsed_body = JSON.parse(@response.body)
+    assert_equal 2, parsed_body.size
+  end
 end
