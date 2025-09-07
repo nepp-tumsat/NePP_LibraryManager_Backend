d1aa9a0e7cfd712a72b492e0d68e1b47bbd7c1b8
Doragon-RH <r216af@icloud.com>
2025-09-07 23:47:24 +0900

    コントローラ、テストを可読性を高め、また例外をキャッチできるように変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,37 +1,54 @@
 # frozen_string_literal: true
 
 class BooksController < ApplicationController
+  before_action :set_book, only: %i[show update destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid,  with: :render_unprocessable
+
   def index
-    books = Book.all
-    render json: books, status: :ok
+    render json: Book.all, status: :ok
   end
 
   def show
-    book = Book.find(params[:id])
-    render json: book, status: :ok
+    render json: @book, status: :ok
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
-      render json: book, status: :created
-    else
-      render json: book.errors, status: :unprocessable_entity
-    end
+    book = Book.create!(book_params)
+    render json: book, status: :created, location: book_url(book)
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-      render json: book, status: :ok
-    else
-      render json: book.errors, status: :unprocessable_entity
-    end
+    @book.update!(book_params)
+    render json: @book, status: :ok
+  end
+
+  def destroy
+    @book.destroy!
+    head :no_content
   end
 
   private
 
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
   def book_params
-    params.require(:book).permit(:title, :author, :description, :cover_image_url, :page_count, :price, :published_date)
+    params.require(:book).permit(
+      :title, :author, :description, :cover_image_url, :page_count, :price, :published_date
+    )
+  end
+
+  # ---- error responders ----
+  def render_not_found(err)
+    render json: { errors: [{ code: 'not_found', detail: err.message }] }, status: :not_found
+  end
+
+  def render_unprocessable(err)
+    render json: {
+      errors: err.record.errors.map { |ve| { code: 'validation_error', attribute: ve.attribute, detail: ve.message } }
+    }, status: :unprocessable_entity
   end
 end
--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -23,15 +23,31 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
   test 'should create book' do
     assert_difference('Book.count') do
       post books_url,
-           params: { book: { title: 'New Book', author: 'New Author', published_date: '2023-01-01',
-                             isbn: '1234567890' } }
+           params: { book: { title: 'New Book', author: 'New Author', published_date: '2023-01-01' } }
     end
+
     assert_response :created
+    body = JSON.parse(@response.body)
+    assert_equal 'New Book', body['title']
+    assert_equal 'New Author', body['author']
+    assert_equal '2023-01-01', body['published_date']
   end
-  test 'should update book' do
-    patch book_url(books(:one)), params: { book: { title: 'Updated Title' } }
-    assert_response :success
-    books(:one).reload
-    assert_equal 'Updated Title', books(:one).title
+
+  test 'create fails without title' do
+    assert_no_difference('Book.count') do
+      post books_url, params: { book: { author: 'X' } }, as: :json
+    end
+    assert_response :unprocessable_entity
+    errors = response.parsed_body
+    assert_includes errors.to_s, 'title'
+  end
+
+  test 'updates title' do
+    book = books(:one)
+
+    patch book_url(book.id), params: { book: { title: 'Updated Title' } }
+    assert_response :ok
+
+    assert_equal 'Updated Title', book.reload.title
   end
 end
