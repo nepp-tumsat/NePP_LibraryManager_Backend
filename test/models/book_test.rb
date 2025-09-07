b3de301c3fd4484c55a54648e34b6d3cfcfe875d
Doragon-RH <r216af@icloud.com>
2025-09-07 23:46:09 +0900

    モデルにnot null制約を追加し、テストの記述を行った

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -3,7 +3,15 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'title is required' do
+    book = Book.new(author: 'X')
+    assert_not book.valid?
+    assert_includes book.errors[:title], "can't be blank"
+  end
+
+  test 'title cannot be whitespace only' do
+    book = Book.new(title: '   ')
+    assert_not book.valid?
+    assert_includes book.errors[:title], "can't be blank"
+  end
 end
