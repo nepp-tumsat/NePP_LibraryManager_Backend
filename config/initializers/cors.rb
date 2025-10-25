034ba59f0f0ef9a67cbb0da62275f2dee581ec20
Doragon-RH <r216af@icloud.com>
2025-10-25 21:12:01 +0900

    rubocop のコード整形

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,18 +3,18 @@
 allowed_origins =
   if Rails.env.production?
     # 例: CORS_ORIGINS="https://app.example.com,https://admin.example.com"
-    ENV.fetch("CORS_ORIGINS").split(",").map { |o| o.strip }
+    ENV.fetch('CORS_ORIGINS').split(',').map(&:strip)
   else
-    "*"
+    '*'
   end
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins allowed_origins
-    resource "*",
+    resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head]
-             # , expose: %w[Authorization]     # Bearerトークンをレスポンスで見せたい場合
-             # , credentials: true              # Cookieを使う場合はtrue（*は不可; 本番は明示ドメイン必須）
+    # , expose: %w[Authorization]     # Bearerトークンをレスポンスで見せたい場合
+    # , credentials: true              # Cookieを使う場合はtrue（*は不可; 本番は明示ドメイン必須）
   end
 end
